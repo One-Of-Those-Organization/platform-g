6b334c4e0a6dc6c364e59b9de69e859f8ccc05f3
Fernando Perry <fernandoperry1234@gmail.com>
2025-09-22 00:07:29 +0700

    split some file

--- main.lua
+++ main.lua
@@ -1,67 +1,20 @@
-local deskScale = 12
-local deskCanvas
-local deskMesh
-local deskTexture
-local deskPolygon
-
--- How many times to repeat texture across trapezoid
-local repeatX, repeatY = 1, 1
+local objects = {
+    require("rtable"),
+}
 
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
     local w,h = love.graphics.getDimensions()
-    deskCanvas = love.graphics.newCanvas(math.floor(w/deskScale), math.floor(h/deskScale))
-
-    -- Load texture (your wood grain image)
-    deskTexture = love.graphics.newImage("assets/desk_texture.jpg")
-    deskTexture:setWrap("repeat", "repeat")
     fadeOut = love.graphics.newShader("fade-out.glsl")
 
-
-    local cw, ch = deskCanvas:getDimensions()
-    local desk_perspective = math.floor(cw * 0.10)
-
-    -- trapezoid corners with repeated UVs
-    local desk = {
-        {desk_perspective,      ch - ch/2.5,  0,        0},        -- top-left
-        {cw - desk_perspective, ch - ch/2.5,  repeatX,  0},        -- top-right
-        {cw,                    ch,           repeatX,  repeatY},  -- bottom-right
-        {0,                     ch,           0,        repeatY},  -- bottom-left
-    }
-
-    deskMesh = love.graphics.newMesh(desk, "fan", "static")
-    deskMesh:setTexture(deskTexture)
-
-    deskPolygon = {
-        desk[1][1], desk[1][2], -- top-left
-        desk[2][1], desk[2][2], -- top-right
-        desk[3][1], desk[3][2], -- bottom-right
-        desk[4][1], desk[4][2], -- bottom-left
-    }
+    for i = 1, #objects do
+        objects[i].preload(fadeOut)
+    end
 end
 
 function love.draw()
-    local g = love.graphics
-    local cw, ch = deskCanvas:getDimensions()
-
-    g.setCanvas(deskCanvas)
-    g.clear(0,0,0,0)
-
-    g.setShader(fadeOut)
-
-        g.setColor(1,1,1)
-        g.draw(deskMesh, cw/2, ch/2, 0, 1, 1, cw/2, ch/2)
-
-    g.setShader()
-
-    g.setLineWidth(1)
-    g.setColor(0.05,0.05,0.05) -- black bezel/outline
-    g.polygon("line", deskPolygon)
-
-    g.setCanvas()
-
-    g.setColor(1,1,1)
-    g.draw(deskCanvas, 0, 0, 0, deskScale, deskScale)
+    for i = 1, #objects do
+        objects[i].render()
+    end
 end
-
---
+++ rtable.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+function M.preload(shaders)
+    local deskScale = 12
+    local repeatX, repeatY = 1, 1
+    local w,h = love.graphics.getDimensions()
+
+    M.deskScale = deskScale
+    M.fadeOut = shaders
+
+    M.deskCanvas = love.graphics.newCanvas(math.floor(w/deskScale), math.floor(h/deskScale))
+
+    M.deskTexture = love.graphics.newImage("assets/desk_texture.jpg")
+    M.deskTexture:setWrap("repeat", "repeat")
+
+    local cw, ch = M.deskCanvas:getDimensions()
+    local desk_perspective = math.floor(cw * 0.10)
+
+    M.desk = {
+        {desk_perspective,      ch - ch/2.5,  0,        0},
+        {cw - desk_perspective, ch - ch/2.5,  repeatX,  0},
+        {cw,                    ch,           repeatX,  repeatY},
+        {0,                     ch,           0,        repeatY},
+    }
+
+    M.deskMesh = love.graphics.newMesh(M.desk, "fan", "static")
+    M.deskMesh:setTexture(M.deskTexture)
+
+    M.deskPolygon = {
+        M.desk[1][1], M.desk[1][2],
+        M.desk[2][1], M.desk[2][2],
+        M.desk[3][1], M.desk[3][2],
+        M.desk[4][1], M.desk[4][2],
+    }
+end
+
+function M.render()
+    local g = love.graphics
+    local cw, ch = M.deskCanvas:getDimensions()
+
+    -- draw onto the canvas
+    g.setCanvas(M.deskCanvas)
+    g.clear(0,0,0,0)
+
+    g.setShader(M.fadeOut)
+    g.setColor(1,1,1)
+    g.draw(M.deskMesh, cw/2, ch/2, 0, 1, 1, cw/2, ch/2)
+    g.setShader()
+
+    g.setLineWidth(1)
+    g.setColor(0.05,0.05,0.05)
+    g.polygon("line", M.deskPolygon)
+
+    g.setCanvas() -- back to screen
+
+    -- scale up the canvas
+    g.setColor(1,1,1)
+    g.draw(M.deskCanvas, 0, 0, 0, M.deskScale, M.deskScale)
+end
+
+return M
